097a3a95d4454170b48ac732d58f1ca59181da11
Robert Stevens <robertstevens2015@gmail.com>
2020-08-07 20:09:43 -0500

    Initial commit, added 10 event listeners!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+const body = document.querySelector('body');
+body.addEventListener('click', (element) => {
+  element.target.style.backgroundColor = 'cyan';
+});
+body.addEventListener('dblclick', (element) => {
+    element.target.style.backgroundColor = 'white';
+});
+const image = document.querySelector('img');
+image.addEventListener('mouseenter', (element) => {
+    console.log('vroom vroom');
+});
+image.addEventListener('mouseleave', (element) => {
+    console.log('skrrrrrt');
+});
+const boldHeader = document.querySelector('h2');
+boldHeader.addEventListener('mouseover', (element) => {
+    element.stopPropagation();
+    element.target.style.color = 'red';
+});
+body.addEventListener('wheel', (element) => {
+    element.target.style.color = 'black';
+});
+body.addEventListener('copy', (element) => {
+    element.stopPropagation();
+    console.log('STOP THAT THIEF!');
+});
+body.addEventListener('paste', (element) => {
+    element.stopPropagation();
+    console.log('There is a time and place for that, but it is neither here, nor now!');
+});
+body.addEventListener('cut', (element) => {
+    element.stopPropagation();
+    console.log('You do not know how to do that');
+});
+body.addEventListener('contextmenu', (element) => {
+    console.log('Stop hitting that button!')
+})
